0cd505773b6148f017e0a3d4c469205d814659ad
Darius Zivertas <2293236+zzzarius@users.noreply.github.com>
2025-05-17 22:21:18 +0300

    Create media image when asset created

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,52 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
+import { type Asset, createClient } from 'contentful-management';
+
+const client = createClient({
+  accessToken: process.env.CONTENTFUL_MANAGEMENT_API_KEY!,
+});
+
+
+async function getEnvironment() {
+  const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID!);
+  const environment = await space.getEnvironment(process.env.CONTENTFUL_ENVIRONMENT_ID!);
+  return environment;
+}
+
+function isAsset(obj: any): obj is Asset {
+  return (
+    obj &&
+    typeof obj === 'object' &&
+    'sys' in obj &&
+    'fields' in obj &&
+    'metadata' in obj &&
+    'type' in obj.sys &&
+    obj.sys.type === 'Asset'
+  );
+}
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   const { body } = req;
   if (!body) {
     return res.status(400).send("No body provided");
   }
-  return res.send(JSON.stringify(body));
+  if (!isAsset(body)) {
+    return res.status(400).send("Invalid asset object");
+  }
+  const env = await getEnvironment();
+  env.createEntryWithId('mediaImage', body.sys.id + '-media', {
+    fields: {
+      internalName: {
+        'en-US': body.fields?.title['en-US'] ?? body.fields?.file?.fields?.fileName ?? 'No name'
+      },
+      image: {
+        'en-US': {
+          sys: {
+            id: body.sys.id,  
+            type: 'Link',
+            linkType: 'Asset'
+          }
+        }
+      }
+    }
+  });
 }
